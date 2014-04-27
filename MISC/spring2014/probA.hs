c8e966069c7e2cebb247c2b75d06946f625c320a
gspindles <joseph.m.ching@gmail.com>
2014-04-27 00:01:52 -0500

    Finished up io part
    The implementation of problem A with Haskell is now complete.

--- MISC/spring2014/probA.hs
+++ MISC/spring2014/probA.hs
@@ -1,15 +1,17 @@
 import Data.List (intersperse)
 import System.Environment (getArgs)
-import System.IO
 
+-- | compile : ghc --make probA.hs
+--          or ghc --make probA
+-- | run     : ./probA int
+
+-- | read an int from first argument
 main :: IO ()
-main = do arg <- head getArgs
-          size <- read arg :: Int
-          putStrLn . makeEPIC . liftIO . size
-
-liftIO :: IO a -> a
-liftIO (IO ()) = 0
-liftIO (IO a)  = a
+main = do args <- getArgs
+          putStrLn . makeEPIC $ (read (head args) :: Int)
+-- | scanner style input version
+-- main = do arg <- getLine
+--           putStrLn . makeEPIC $ (read arg :: Int)
 
 -- | make a single block
 makeBlock :: Int -> String -> String
@@ -21,82 +23,82 @@ makeType1 n = makeBlock 5 . makeBlock n
 
 -- | make a X____ block
 makeType2 :: Int -> String -> String
-makeType2 n letter = text ++ spaces
-  where text   = makeBlock n letter
+makeType2 n l = text ++ spaces
+  where text   = makeBlock n l
         spaces = makeBlock (4 * n) " "
 
 -- | make a X___X block
 makeType3 :: Int -> String -> String
-makeType3 n letter = concat [text, spaces, text]
-  where text   = makeBlock n letter
+makeType3 n l = concat [text, spaces, text]
+  where text   = makeBlock n l
         spaces = makeBlock (3 * n) " "
 
 -- | make a __X__ block
 makeType4 :: Int -> String -> String
-makeType4 n letter = concat [spaces, text, spaces]
+makeType4 n l = concat [spaces, text, spaces]
   where spaces = makeBlock (2 * n) " "
-        text   = makeBlock n letter
+        text   = makeBlock n l
 
--- | make letter
+-- | make l
 makeLetter :: Int -> String -> [(String -> String)] -> String
-makeLetter n letter makeFuncs = join "\n" expandRows 
-  where rows       = zipWith id makeFuncs . repeat $ letter
+makeLetter n l gfs = join "\n" expandRows 
+  where rows       = zipWith id gfs . repeat $ l
         expandRows = map (join "\n"  . take n . repeat) rows
 
 -- | make an E
 makeE :: Int -> String
-makeE n = makeLetter n "E" makeFuncs
-  where makeFuncs = [ makeType1 n  -- XXXXX
-                    , makeType2 n  -- X____
-                    , makeType1 n  -- XXXXX
-                    , makeType2 n  -- X____
-                    , makeType1 n  -- XXXXX
-                    ]
- 
+makeE n = makeLetter n "E" gfs
+  where gfs = [ makeType1 n  -- XXXXX
+              , makeType2 n  -- X____
+              , makeType1 n  -- XXXXX
+              , makeType2 n  -- X____
+              , makeType1 n  -- XXXXX
+              ]
+
 -- | make an P
 makeP :: Int -> String
-makeP n = makeLetter n "P" makeFuncs
-  where makeFuncs = [ makeType1 n  -- XXXXX
-                    , makeType3 n  -- X___X
-                    , makeType1 n  -- XXXXX
-                    , makeType2 n  -- X____
-                    , makeType2 n  -- X____
-                    ]
+makeP n = makeLetter n "P" gfs
+  where gfs = [ makeType1 n  -- XXXXX
+              , makeType3 n  -- X___X
+              , makeType1 n  -- XXXXX
+              , makeType2 n  -- X____
+              , makeType2 n  -- X____
+              ]
 
 -- | make a I
 makeI :: Int -> String
-makeI n = makeLetter n "I" makeFuncs
-  where makeFuncs = [ makeType1 n  -- XXXXX
-                    , makeType4 n  -- __X__
-                    , makeType4 n  -- __X__
-                    , makeType4 n  -- __X__
-                    , makeType1 n  -- XXXXX
-                    ]
+makeI n = makeLetter n "I" gfs
+  where gfs = [ makeType1 n  -- XXXXX
+              , makeType4 n  -- __X__
+              , makeType4 n  -- __X__
+              , makeType4 n  -- __X__
+              , makeType1 n  -- XXXXX
+              ]
 
 -- | make a C
 makeC :: Int -> String
-makeC n = makeLetter n "C" makeFuncs
-  where makeFuncs = [ makeType1 n  -- XXXXX
-                    , makeType2 n  -- X____
-                    , makeType2 n  -- X____
-                    , makeType2 n  -- X____
-                    , makeType1 n  -- XXXXX
-                    ]
+makeC n = makeLetter n "C" gfs
+  where gfs = [ makeType1 n  -- XXXXX
+              , makeType2 n  -- X____
+              , makeType2 n  -- X____
+              , makeType2 n  -- X____
+              , makeType1 n  -- XXXXX
+              ]
 
 -- | make new lines
 makeNewLines :: Int -> String
-makeNewLines n = makeBlock n "\n"
+makeNewLines n = makeBlock (n+1) "\n"
 
 -- | make epic
 makeEPIC :: Int -> String
-makeEPIC 0 = "EPIC"
+makeEPIC 0 = join "\n" ["E", "P", "I", "C"]
 makeEPIC n = join (makeNewLines n) . epic $ n
   where epic = zipWith id [makeE, makeP, makeI, makeC] . repeat
 
--- | repeat the row scale times
+-- | repeat the row n times
 expandRows :: Int ->  String -> String
 expandRows n = join "\n" . take n . repeat
 
 -- | join strings with delimiter
 join :: [a] -> [[a]] -> [a]
-join delim = concat . intersperse delim
+join d = concat . intersperse d
